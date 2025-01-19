a1652d9fccfced2abaf40744fd8290560fec8b5a
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2025-01-20 02:23:30 +0700

    TransactionService Extensions

---
+++ Server/TransactionService/src/TransactionService.Domain/DomainServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransactionService.Domain.Factories;
+using TransactionService.Domain.Services;
+
+namespace TransactionService.Domain
+{
+    public static class DomainServiceExtensions
+    {
+        public static IServiceCollection AddDomainServices(this IServiceCollection services)
+        {
+            services.AddScoped<ITransactionHistoryDomainService, TransactionDomainHistoryService>();
+            services.AddScoped<ITransactionHistoryFactory, TransactionHistoryFactory>();
+            return services;
+        }
+    }
+}
