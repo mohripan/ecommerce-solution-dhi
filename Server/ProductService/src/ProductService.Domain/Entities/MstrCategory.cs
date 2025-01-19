bd3e332e4c4ffe324cf38e95a2315cb019fc81e1
Mohammad Ripan Saiful Mansur <42226480+mohripan@users.noreply.github.com>
2025-01-19 15:33:13 +0700

    Product Service Database Design

---
+++ Server/ProductService/src/ProductService.Domain/Entities/MstrCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductService.Domain.Entities
+{
+    public class MstrCategory
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; } = default!;
+        public DateTime CreatedOn { get; set; } = new DateTime(2025, 1, 1);
+        public string CreatedBy { get; set; } = "SYS";
+        public DateTime? ModifiedOn { get; set; }
+        public string? ModifiedBy { get; set; }
+    }
+}
---
+++ Server/ProductService/src/ProductService.Domain/Entities/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductService.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        public int UserId { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+        public string CreatedBy { get; set; } = default!;
+        public DateTime? ModifiedOn { get; set; }
+        public string? ModifiedBy { get; set; }
+
+        public MstrCategory? Category { get; set; }
+    }
+}
